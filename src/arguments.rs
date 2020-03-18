a23d712607bbd783d56fc53277a23429da78fd1f
Eloy <zer1t0ps@protonmail.com>
2020-03-18 09:29:21 +0100

    extract App name, version, author and about from Cargo.toml

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,9 +1,10 @@
 use clap::{App, Arg};
 
 fn args() -> App<'static, 'static> {
-    App::new("http_or_https")
-    .version("0.1")
-    .about("Check if http or https works for targets")
+    App::new(env!("CARGO_PKG_NAME"))
+	.version(env!("CARGO_PKG_VERSION"))
+	.author(env!("CARGO_PKG_AUTHORS"))
+	.about(env!("CARGO_PKG_DESCRIPTION"))
     .arg(
         Arg::with_name("hosts-file")
         .takes_value(true)
